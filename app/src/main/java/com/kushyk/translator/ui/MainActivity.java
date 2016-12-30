d7f1162fd0796f1ecfa3890634c91f828f5664cc
Iurii Kushyk <xaocuk1@gmail.com>
2016-12-30 03:09:09 +0200

    Implement TranslationManager

--- app/src/main/java/com/kushyk/translator/ui/MainActivity.java
+++ app/src/main/java/com/kushyk/translator/ui/MainActivity.java
@@ -3,7 +3,6 @@ package com.kushyk.translator.ui;
 import android.os.Bundle;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -11,13 +10,13 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.api.services.translate.model.LanguagesListResponse;
-import com.google.api.services.translate.model.LanguagesResource;
-import com.google.api.services.translate.model.TranslationsListResponse;
-import com.google.common.collect.Lists;
+import com.kushyk.translator.OfflineTranslatorService;
 import com.kushyk.translator.R;
-import com.kushyk.translator.TranslatorService;
+import com.kushyk.translator.ReconnectException;
+import com.kushyk.translator.TranslateManager;
+import com.kushyk.translator.OnlineTranslatorService;
 
 import java.util.List;
 
@@ -26,7 +25,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnEditorAction;
 import butterknife.Unbinder;
-import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -37,21 +35,22 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.outLanguageView)
     Spinner outLanguageView;
     private Unbinder unbinder;
-    private TranslatorService translateService;
+    private TranslateManager translateManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
-        translateService = new TranslatorService();
+        translateManager = new TranslateManager(new OnlineTranslatorService(),new OfflineTranslatorService());
         getLanguages();
     }
 
     @OnEditorAction(R.id.translateEditView)
     public boolean onEditorAction(TextView view, int actionId, KeyEvent event){
         if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-            translate(view.getText().toString(), getTargetLanguageCode());
+            translate(view.getText().toString(),getCurrentLanguageCode(), getTargetLanguageCode());
             return true;
         }
         return false;
@@ -61,44 +60,57 @@ public class MainActivity extends AppCompatActivity {
         return outLanguageView.getSelectedItem().toString();
     }
 
-    private void translate(String text, String targetLanguage) {
+    private String getCurrentLanguageCode() {
+        return inLanguageView.getSelectedItem().toString();
+    }
+
+    private void translate(String text, String currentLanguage, String targetLanguage) {
         if (text.isEmpty()) {
             showToast(R.string.put_some_text);
             return;
         }
-        Observable.fromCallable(() -> translateService.translate(text, targetLanguage))
+        translateManager.translate(text, currentLanguage, targetLanguage)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::onSuccessTranslate, this::onErrorTranslate);
     }
 
-    private void onSuccessTranslate(TranslationsListResponse translationsListResponse) {
-        Log.d(LOG_TAG, translationsListResponse.toString());
-        String[] translateTexts = Lists.transform(translationsListResponse.getTranslations(), res -> res.getTranslatedText()).toArray(new String[0]);
-        AlertDialog.Builder  builder= new AlertDialog.Builder(this);
-        builder.setItems(translateTexts, null);
-        builder.create().show();
+    private void onSuccessTranslate(String[] translateTexts) {
+        Log.d(LOG_TAG, translateTexts.toString());
+        if (translateTexts.length == 0) {
+            showToast(getString(R.string.no_translation));
+            return;
+        }
+        showToast(translateTexts[0]);
     }
 
     private void onErrorTranslate(Throwable throwable) {
         Log.e(LOG_TAG, "onErrorTranslate()",throwable);
         showToast(R.string.cant_translate);
+        checkReconection(throwable);
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 
     private void getLanguages() {
-        Observable.fromCallable(()->translateService.languages())
+        translateManager.getLanguages()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::onSuccessGetLanguage, this::onErrorGetLanguage);
     }
 
-    private void onSuccessGetLanguage(LanguagesListResponse languagesListResponse) {
-        fillSpinners(languagesListResponse);
+    private void onSuccessGetLanguage(List<String> languages) {
+        fillSpinners(languages);
     }
 
     private void onErrorGetLanguage(Throwable throwable) {
         Log.e(LOG_TAG, "onErrorTranslate()",throwable);
-        showToast(R.string.server_error);
+        if (!(throwable instanceof ReconnectException)) {
+            showToast(R.string.server_error);
+        }
+        checkReconection(throwable);
     }
 
     @OnClick(R.id.translateIconView)
@@ -110,8 +122,7 @@ public class MainActivity extends AppCompatActivity {
         inLanguageView.setSelection(outPosition);
     }
 
-    private void fillSpinners(LanguagesListResponse languagesListResponse) {
-        List<String> languages = Lists.transform(languagesListResponse.getLanguages(), LanguagesResource::getLanguage);
+    private void fillSpinners(List<String> languages) {
 
         ArrayAdapter<String> adapterIn = new ArrayAdapter(this, android.R.layout.simple_spinner_item, languages);
         ArrayAdapter<String> adapterOut = new ArrayAdapter(this, android.R.layout.simple_spinner_item, languages);
@@ -119,11 +130,17 @@ public class MainActivity extends AppCompatActivity {
         inLanguageView.setAdapter(adapterIn);
         inLanguageView.setSelection(0);
         outLanguageView.setAdapter(adapterOut);
-        outLanguageView.setSelection(0);
+        outLanguageView.setSelection(1);
     }
 
     private void showToast(@StringRes int textResId) {
         Snackbar.make(outLanguageView, textResId, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void checkReconection(Throwable throwable) {
+        if (throwable instanceof ReconnectException) {
+            getLanguages();
+        }
     }
 
     @Override
